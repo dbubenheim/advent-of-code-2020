fd98d5fb8f18d75c9b15bbaa5e4cd932a3b4a90e
Daniel Bubenheim <daniel.bubenheim@gmail.com>
2020-12-20 00:15:34 +0100

    adds a test for day 06

---
+++ app/src/test/kotlin/advent/of/code/Day06Test.kt
@@ -0,0 +1,19 @@
+package advent.of.code
+
+import advent.of.code.Day06.Companion.customCustomsPart1
+import advent.of.code.Day06.Companion.customCustomsPart2
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+internal class Day06Test {
+
+    @Test
+    fun testPart1() {
+        assertThat(customCustomsPart1()).isEqualTo(6583)
+    }
+
+    @Test
+    fun testPart2() {
+        assertThat(customCustomsPart2()).isEqualTo(3290)
+    }
+}
