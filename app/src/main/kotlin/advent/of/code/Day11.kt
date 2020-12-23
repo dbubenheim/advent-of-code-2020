dbea9ccd1a552591a11e1925855b5be2bb1e0847
Daniel Bubenheim <daniel.bubenheim@gmail.com>
2020-12-23 16:49:25 +0100

    adds directories for all upcoming problems. adds a travis file for ci builds

---
+++ app/src/main/kotlin/advent/of/code/Day11.kt
@@ -0,0 +1,25 @@
+package advent.of.code
+
+internal class Day11 {
+
+    companion object {
+
+        @JvmStatic
+        fun part1() : Long {
+
+            return 0L
+        }
+
+        @JvmStatic
+        fun part2(): Long {
+
+            return 0L
+        }
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("part1: ${part1()}")
+            println("part2: ${part2()}")
+        }
+    }
+}
---
+++ app/src/test/kotlin/advent/of/code/Day11Test.kt
@@ -0,0 +1,19 @@
+package advent.of.code
+
+import advent.of.code.Day11.Companion.part1
+import advent.of.code.Day11.Companion.part2
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+internal class Day11Test {
+
+    @Test
+    fun testPart1() {
+        assertThat(part1()).isEqualTo(42)
+    }
+
+    @Test
+    fun testPart2() {
+        assertThat(part2()).isEqualTo(42)
+    }
+}
