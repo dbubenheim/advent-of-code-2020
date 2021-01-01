5578f573fe3c43a944e5510539fcdd8d881bb53e
Daniel Bubenheim <daniel.bubenheim@gmail.com>
2021-01-01 23:07:46 +0100

    adds a solution to day07 part1 and some tests.

--- app/src/test/kotlin/advent/of/code/day07/Day07Test.kt
+++ app/src/test/kotlin/advent/of/code/day07/Day07Test.kt
@@ -1,6 +1,13 @@
 package advent.of.code.day07
 
+import advent.of.code.day07.Day07.Companion.part1
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 internal class Day07Test {
 
-
+    @Test
+    fun testPart1() {
+        assertThat(part1()).isEqualTo(238)
+    }
 }
